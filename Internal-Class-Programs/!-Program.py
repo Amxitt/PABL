7840a7ffd6dd5fb1a747c22fe9e88996fe0f7f84
Amxitt <amxittt@gmail.com>
2026-01-17 10:09:58 +0000

    Initial commit

--- Internal-Class-Programs/!-Program.py
+++ Internal-Class-Programs/!-Program.py
@@ -1,2 +1,4 @@
 print("This is my first session of PABL Class.")
 print("Here we are going to perform competitive coding.")
+
+#git branch (command= git branch -M main) purpose: to merge in main branch
